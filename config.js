130cc369a0925a332c56e1132617f96cd2ab4dc9
lc100068-cmd <lc100068@gmail.com>
2026-09-25 20:48:05 +0900

    Set Apps Script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Apps Script の「ウェブアプリ URL」をここに貼り付けてください（SETUP.md の手順 3）
 // 例: "https://script.google.com/macros/s/XXXXXXXXXXXX/exec"
-window.SEINEN_API_URL = "";
+window.SEINEN_API_URL = "https://script.google.com/macros/s/AKfycbxf2EcK0Uw0_pabqZ3F0D0iWKa_4Dixus9grJe9N__cJIIt1fsJEEMhmq414LqPPI_juw/exec";
